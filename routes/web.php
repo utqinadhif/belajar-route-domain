a1a740ae3253839184617600a732628a162072a3
utqinadhif <nadhifbeo@gmail.com>
2024-03-25 08:42:40 +0700

    try

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::domain('{name}.'.config('app.url'))->group(function () {
     Route::get('/', function (string $name) {
-        if ($name == '_acme-challenge') {
-            return 'w-MJzR9KGn_QAINwmVZyzPWpwBh0O-OLrS-W0TMyqFQ';
-        }
-
         return $name;
     });
 });
